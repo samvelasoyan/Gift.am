8e8ed380a7d8601ddcffad06ec305b7027f3f926
Samvel Asoyan <samvel24.1998@gmail.com>
2019-12-09 18:10:00 +0400

    new categories

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 import Box from "./Box";
 import background from "../images/IMG_14224.jpg";
 import jewelryBG from "../images/categories/jewelry1.jpg";
@@ -7,6 +7,9 @@ import beverageBG from "../images/categories/drinks.jpeg";
 import watchesBG from "../images/categories/watches1.jpg";
 import perfumeBG from "../images/categories/perfume.jpg";
 import teddybearBG from "../images/categories/teddybear.jpg";
+import balloonsBG from "../images/categories/balloons1.jpg";
+import cakeBG from "../images/categories/cakes.jpg";
+import basketBG from "../images/categories/basket2.jpg";
 
 export default function Categories() {
     return (
@@ -18,10 +21,13 @@ export default function Categories() {
             <div className="categories" id="categories">
                 <Box background={perfumeBG} text="perfume" />
                 <Box background={jewelryBG} text="jewelry" />
-                <Box background={flowersBG} text="flowers" />
-                <Box background={beverageBG} text="beverage" />
                 <Box background={watchesBG} text="watches" />
+                <Box background={flowersBG} text="flowers" />
                 <Box background={teddybearBG} text="teddy bear" />
+                <Box background={beverageBG} text="beverage" />
+                <Box background={cakeBG} text="cakes" />
+                <Box background={basketBG} text="hampers" />
+                <Box background={balloonsBG} text="balloons" />
             </div>
         </div>
     );
